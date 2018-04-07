b27177ab5bbeed7b7ffb0dbab4f86ea72f97b5f8
mauvi <mauriciojvruiz@gmail.com>
2018-04-07 00:22:47 -0600

    Game

--- MainActivity.java
+++ MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.mauvi.myapplication;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.media.Image;
 import android.os.CountDownTimer;
+import android.os.Handler;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,15 +13,39 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Random;
 import static com.example.mauvi.myapplication.R.color.colorPrimary;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageButton im1,im2,im3,im4,im5,im6,im7,im8,im9;
+    Integer[] cartasArreglo = {10,11,12,13,14,15,16,17,18};
+    int image1,image2,image3,image4,image5,image6,image7,image8,image9;
+
+    int primeraCarta, segundaCarta;
+    int primerClick, segundoClick;
+    int numeroCarta = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        im1 = (ImageButton) findViewById(R.id.imageButton1);im1.setTag("1");
+        im2 = (ImageButton) findViewById(R.id.imageButton2);im2.setTag("2");
+        im3 = (ImageButton) findViewById(R.id.imageButton3);im3.setTag("3");
+        im4 = (ImageButton) findViewById(R.id.imageButton4);im4.setTag("4");
+        im5 = (ImageButton) findViewById(R.id.imageButton5);im5.setTag("5");
+        im6 = (ImageButton) findViewById(R.id.imageButton6);im6.setTag("6");
+        im7 = (ImageButton) findViewById(R.id.imageButton7);im7.setTag("7");
+        im8 = (ImageButton) findViewById(R.id.imageButton8);im8.setTag("8");
+        im9 = (ImageButton) findViewById(R.id.imageButton9);im9.setTag("9");
+
+        cargaCartas();
+        Collections.shuffle(Arrays.asList(cartasArreglo));
     }
 
     @Override
@@ -42,6 +68,18 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void cargaCartas(){
+        image1= R.drawable.calabaza;
+        image2= R.drawable.sandia;
+        image3= R.drawable.salchicha;
+        image4= R.drawable.pollo;
+        image5= R.drawable.piza;
+        image6= R.drawable.pescado;
+        image7= R.drawable.papas;
+        image8= R.drawable.huevo;
+        image9= R.drawable.hamburguesa;
+    }
+
     private void juegoAvanzado() {
         Intent intent=new Intent(this, Main2Activity.class);
         startActivity(intent);
@@ -52,39 +90,48 @@ public class MainActivity extends AppCompatActivity {
         Contador contador  = new Contador( 2000,button);;
         switch (view.getId()){
             case R.id.imageButton1:
-                button.setImageResource(R.drawable.pescado);
+                int carta = Integer.parseInt((String) view.getTag());
+                desarrolloJuego(im1,carta);
                 contador.start();
                 break;
             case R.id.imageButton2:
-                button.setImageResource(R.drawable.calabaza);
+                carta = Integer.parseInt((String) view.getTag());
+                desarrolloJuego(im2,carta);
                 contador.start();
                 break;
             case R.id.imageButton3:
-                button.setImageResource(R.drawable.hamburguesa);
+                carta = Integer.parseInt((String) view.getTag());
+                desarrolloJuego(im3,carta);
                 contador.start();
                 break;
             case R.id.imageButton4:
-                button.setImageResource(R.drawable.huevo);
+                carta = Integer.parseInt((String) view.getTag());
+                desarrolloJuego(im4,carta);
                 contador.start();
                 break;
             case R.id.imageButton5:
-                button.setImageResource(R.drawable.piza);
+                carta = Integer.parseInt((String) view.getTag());
+                desarrolloJuego(im5,carta);
                 contador.start();
                 break;
             case R.id.imageButton6:
-                button.setImageResource(R.drawable.huevo);
+                carta = Integer.parseInt((String) view.getTag());
+                desarrolloJuego(im6,carta);
                 contador.start();
                 break;
             case R.id.imageButton7:
-                button.setImageResource(R.drawable.hamburguesa);
+                carta = Integer.parseInt((String) view.getTag());
+                desarrolloJuego(im7,carta);
                 contador.start();
                 break;
             case R.id.imageButton8:
-                button.setImageResource(R.drawable.calabaza);
+                carta = Integer.parseInt((String) view.getTag());
+                desarrolloJuego(im8,carta);
                 contador.start();
                 break;
             case R.id.imageButton9:
-                button.setImageResource(R.drawable.pescado);
+                carta = Integer.parseInt((String) view.getTag());
+                desarrolloJuego(im9,carta);
                 contador.start();
                 break;
             default:
@@ -93,17 +140,112 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public int verificaNumerosRandom(){
-        Random rand = new Random();
-        int n1,n2,n3,n4,n5;
-        boolean caso=true;
-        n1 = rand.nextInt(9);
-        n2 = rand.nextInt(9);
-        n3 = rand.nextInt(9);
-        n4 = rand.nextInt(9);
-        n5 = rand.nextInt(9);
-        
-        return 0;
+    public void desarrolloJuego(ImageButton image,int carta) {
+        if(cartasArreglo[carta] == 10){image.setImageResource(image1);      //1
+        }else if(cartasArreglo[carta] == 11){image.setImageResource(image2);//2
+        }else if(cartasArreglo[carta] == 12){image.setImageResource(image3);//3
+        }else if(cartasArreglo[carta] == 13){image.setImageResource(image4);//1
+        }else if(cartasArreglo[carta] == 14){image.setImageResource(image5);//2
+        }else if(cartasArreglo[carta] == 15){image.setImageResource(image6);//3
+        }else if(cartasArreglo[carta] == 16){image.setImageResource(image7);//1
+        }else if(cartasArreglo[carta] == 17){image.setImageResource(image8);//2
+        }else if(cartasArreglo[carta] == 18){image.setImageResource(image9);}//3
+
+        if(numeroCarta == 1){
+            primeraCarta=cartasArreglo[carta];
+            if(primeraCarta > 13){
+                primeraCarta = primeraCarta - 3;
+            }
+            numeroCarta = 2;
+            primerClick = carta;
+
+            image.setEnabled(false);
+        }else if (numeroCarta == 2){
+            segundaCarta=cartasArreglo[carta];
+            if(segundaCarta > 13){
+                segundaCarta = segundaCarta - 3;
+            }
+            numeroCarta = 1;
+            segundoClick = carta;
+
+            im1.setEnabled(false);im2.setEnabled(false);
+            im3.setEnabled(false);im4.setEnabled(false);
+            im5.setEnabled(false);im6.setEnabled(false);
+            im7.setEnabled(false);im8.setEnabled(false);
+            im9.setEnabled(false);
+
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    calcular();
+                }
+            }, 1000);
+        }
+    }
+
+    private void calcular(){
+        if(primeraCarta==segundaCarta){
+            if(primerClick==0){im1.setVisibility(View.INVISIBLE);
+            }else if(primerClick==1){im2.setVisibility(View.INVISIBLE);
+            }else if(primerClick==2){im3.setVisibility(View.INVISIBLE);
+            }else if(primerClick==3){im4.setVisibility(View.INVISIBLE);
+            }else if(primerClick==4){im5.setVisibility(View.INVISIBLE);
+            }else if(primerClick==5){im6.setVisibility(View.INVISIBLE);
+            }else if(primerClick==6){im7.setVisibility(View.INVISIBLE);
+            }else if(primerClick==7){im8.setVisibility(View.INVISIBLE);
+            }else if(primerClick==8){im9.setVisibility(View.INVISIBLE);}
+
+
+            if(segundoClick==0){im1.setVisibility(View.INVISIBLE);
+            }else if(segundoClick==1){im2.setVisibility(View.INVISIBLE);
+            }else if(segundoClick==2){im3.setVisibility(View.INVISIBLE);
+            }else if(segundoClick==3){im4.setVisibility(View.INVISIBLE);
+            }else if(segundoClick==4){im5.setVisibility(View.INVISIBLE);
+            }else if(segundoClick==5){im6.setVisibility(View.INVISIBLE);
+            }else if(segundoClick==6){im7.setVisibility(View.INVISIBLE);
+            }else if(segundoClick==7){im8.setVisibility(View.INVISIBLE);
+            }else if(segundoClick==8){im9.setVisibility(View.INVISIBLE);}
+        }else {
+            im1.setImageResource(R.drawable.cubiertos);
+            im2.setImageResource(R.drawable.cubiertos);
+            im3.setImageResource(R.drawable.cubiertos);
+            im4.setImageResource(R.drawable.cubiertos);
+            im5.setImageResource(R.drawable.cubiertos);
+            im6.setImageResource(R.drawable.cubiertos);
+            im7.setImageResource(R.drawable.cubiertos);
+            im8.setImageResource(R.drawable.cubiertos);
+            im9.setImageResource(R.drawable.cubiertos);
+        }
+
+        im1.setEnabled(true);
+        im2.setEnabled(true);
+        im3.setEnabled(true);
+        im4.setEnabled(true);
+        im5.setEnabled(true);
+        im6.setEnabled(true);
+        im7.setEnabled(true);
+        im8.setEnabled(true);
+        im9.setEnabled(true);
+
+        salir();
+    }
+
+    public void salir(){
+        if (im1.getVisibility() == View.INVISIBLE &&
+                im2.getVisibility() == View.INVISIBLE &&
+                im3.getVisibility() == View.INVISIBLE &&
+                im4.getVisibility() == View.INVISIBLE &&
+                im5.getVisibility() == View.INVISIBLE &&
+                im6.getVisibility() == View.INVISIBLE &&
+                im7.getVisibility() == View.INVISIBLE &&
+                im8.getVisibility() == View.INVISIBLE &&
+                im9.getVisibility() == View.INVISIBLE ){
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+            alertDialogBuilder.setMessage("JUEGO TERMINADO! \n" );
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
+        }
     }
 
     public void reiniciarFigura(ImageButton button){
